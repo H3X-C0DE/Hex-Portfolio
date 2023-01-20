c104fcbf01260247e7a9038a0d1bf84aa12033d3
IIHEXYII <vetletessem@gmail.com>
2023-01-20 11:04:23 +0100

    added new files and minor changes to code

---
+++ src/Components/Cv.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Cv() {
+  return (
+    <section id="top-cv">
+      <div className="cv-container">
+        <h2 className="cv-title">Cv</h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Social from "./Social";
+import "../Assets/Styles/footer.css";
+
+export default function Footer() {
+  return (
+    <section id="top-Footer">
+      <div className="footer-container">
+        <Social />
+        <div className="page-end">
+          <span>© 2023 Vetle Tessem</span>
+        </div>
+      </div>
+    </section>
+  );
+}
