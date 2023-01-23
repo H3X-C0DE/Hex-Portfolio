bd0977d96ca23e8de5d535c7b9764dca5851562a
IIHEXYII <vetletessem@gmail.com>
2023-01-23 15:22:53 +0100

    Updated TODO's

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-//TODO: add Navbar & Contact from.
+//TODO: add Contact from.
+//TODO: add Cv from.
 //TODO: add / convert javascript functions.
 import "./Styles/App.css";
 
-// import Navbar from "./Components/Contact";
 import HeroSection from "./Components/HeroSection";
 import AnimatedBg from "./Components/misc/AnimatedBg";
 import About from "./Components/About";
@@ -10,14 +10,14 @@ import Projects from "./Components/Projects";
 // import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-// import Cv from "./Components/Cv";
+import Cv from "./Components/Cv";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <HeroSection />
       <About />
-      {/* <Cv /> */}
+      <Cv />
       <Projects />
       {/* <Contact /> */}
       <Footer />
