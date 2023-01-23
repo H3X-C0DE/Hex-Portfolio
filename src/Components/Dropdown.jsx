010b644ca09223f8ee49e6db4ab03da68ef31977
IIHEXYII <vetletessem@gmail.com>
2023-01-23 14:21:09 +0100

    dropdown menu now works

---
+++ src/Components/Dropdown.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useRef } from "react";
+import "../Styles/Navbar.css";
+export default function Dropdown() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive(!isActive);
+
+  return (
+    <div className="menu-container">
+      <button onClick={onClick} className="menu-trigger">
+        <span>&lt; MENU &#47;&gt;</span>
+      </button>
+      <nav
+        ref={dropdownRef}
+        className={`menu ${isActive ? "active" : "inactive"}`}
+      >
+        <ul>
+          <li>
+            <a className="navbar-link" href="#top-home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="#top-about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="#top-portfolio">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a className="navbar-link" href="#top-contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
