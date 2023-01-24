8c89a53f2f86f937c89214ee51e09faf5035e339
IIHEXYII <vetletessem@gmail.com>
2023-01-24 13:54:04 +0100

    made CV-cards for exp & education

--- src/Components/Cv.jsx
+++ src/Components/Cv.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "../Styles/Cv.css";
-import img1 from "../Assets/Images/me.jpg";
+import placeholderImg from "../Assets/Images/me.jpg";
+import img1 from "../Assets/Images/ecplay.jpg";
+import img2 from "../Assets/Images/noroff.jpg";
+
 export default function Cv() {
   return (
     <section id="top-cv">
@@ -10,9 +13,25 @@ export default function Cv() {
           <div className="cv-card">
             <img src={img1} alt="company logo" />
             <div className="cv-card-content">
+              <h3>Junior front-end developer</h3>
+              <p className="cv-card-under-title">
+                EC-Play <span>·</span> Sommer Vikar
+              </p>
+              <p className="cv-card-info">Jun 2021 - Aug 2021 · 3 mos</p>
+              <p className="cv-card-info">Bergen, Norge</p>
+              <p>
+                Jeg lagde componenter til web-Appen deres i typescript Og jobbet
+                med design til sider som ikke var laget enda som for ekempel
+                email verification landings side og lignende.
+              </p>
+            </div>
+          </div>
+          <div className="cv-card">
+            <img src={placeholderImg} alt="company logo" />
+            <div className="cv-card-content">
               <h3>Jobb title</h3>
               <p className="cv-card-under-title">
-                Company <span>Stilling</span>
+                Company <span>·</span> Stilling
               </p>
               <p className="cv-card-info">Jun 2021 - Aug 2021 · 3 mos</p>
               <p className="cv-card-info">Bergen, Norge</p>
@@ -24,21 +43,22 @@ export default function Cv() {
               </p>
             </div>
           </div>
-          <div className="cv-card">
-            <img src={img1} alt="company logo" />
-            <div className="cv-card-content">
-              <h3>Jobb title</h3>
-              <p className="cv-card-under-title">
-                Company <span>Stilling</span>
-              </p>
-              <p className="cv-card-info">Jun 2021 - Aug 2021 · 3 mos</p>
-              <p className="cv-card-info">Bergen, Norge</p>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus optio dicta dolor tenetur eius. Minus doloribus
-                magnam, aliquid ratione et dicta! Aperiam autem harum illum
-                doloremque labore fugit.
-              </p>
+          <h2 className="cv-title">Utdanning</h2>
+          <div className="utdanning">
+            <div className="cv-card">
+              <img src={img2} alt="company logo" />
+              <div className="cv-card-content">
+                <h3>Noroff</h3>
+                <p className="cv-card-under-title">
+                  University <span>·</span> Front-End Development
+                </p>
+                <p className="cv-card-info">Aug 2019 - May 2021</p>
+                <p className="cv-card-info">Bergen, Norge</p>
+                <p>
+                  Lærte basic nivå i design, HTML5, CSS, Javascript, React,
+                  Rest-API, Word-press, og Typography bare for å nevne noe.
+                </p>
+              </div>
             </div>
           </div>
         </div>
