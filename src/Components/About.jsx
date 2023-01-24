5d64d5e923807047b2f152841d3d808c1b70546b
IIHEXYII <vetletessem@gmail.com>
2023-01-24 13:55:17 +0100

    Fixed placements and organized layout better

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -19,37 +19,36 @@ export default function About() {
             arbeid som fotografering, design og lignende. På fritiden liker jeg
             å spille dataspill med venner på nett og se serier.
           </p>
-          <div className="tag-container">
-            <h3 className="tag-title">Tools of trade</h3>
-            <div className="tag-content">
-              <div className="tag tag-teal">#Visual Studio Code</div>
-              <div className="tag tag-teal">#Adobe PhotoShop</div>
-              <div className="tag tag-teal">#Adobe Illustrator</div>
-              <div className="tag tag-teal">#Figma</div>
-            </div>
+        </div>
+        <div className="tag-container">
+          <h3 className="tag-title">Tools of trade</h3>
+          <div className="tag-content">
+            <div className="tag tag-teal">#Visual Studio Code</div>
+            <div className="tag tag-teal">#Adobe PhotoShop</div>
+            <div className="tag tag-teal">#Adobe Illustrator</div>
+            <div className="tag tag-teal">#Figma</div>
+          </div>
 
-            <h3 className="tag-title">Skills</h3>
-            <div className="tag-content">
-              <div className="tag tag-purple">#HTML5</div>
-              <div className="tag tag-purple">#CSS</div>
-              <div className="tag tag-purple">#SCSS</div>
-              <div className="tag tag-purple">#UI & UX</div>
-              <div className="tag tag-purple">#Web Design</div>
-              <div className="tag tag-purple">#Javascript</div>
-              <div className="tag tag-purple">#React</div>
-            </div>
+          <h3 className="tag-title">Skills</h3>
+          <div className="tag-content">
+            <div className="tag tag-purple">#HTML5</div>
+            <div className="tag tag-purple">#CSS</div>
+            <div className="tag tag-purple">#SCSS</div>
+            <div className="tag tag-purple">#UI & UX</div>
+            <div className="tag tag-purple">#Javascript</div>
+            <div className="tag tag-purple">#React</div>
+          </div>
 
-            <h3 className="tag-title">Learning</h3>
-            <div className="tag-content">
-              {/* <div className="tag tag-pink">#HTML5</div>
+          <h3 className="tag-title">Learning</h3>
+          <div className="tag-content">
+            {/* <div className="tag tag-pink">#HTML5</div>
               <div className="tag tag-pink">#CSS</div> */}
-              {/* <div className="tag tag-pink">#Javascript</div> */}
-              <div className="tag tag-pink">#Basic Human Decency</div>
-              <div className="tag tag-pink">#Rest Api</div>
-              <div className="tag tag-pink">Tailwind</div>
-              <div className="tag tag-pink">MongoDB</div>
-              <div className="tag tag-pink">Node.js/Express</div>
-            </div>
+            {/* <div className="tag tag-pink">#Javascript</div> */}
+            <div className="tag tag-pink">#Basic Human Decency</div>
+            <div className="tag tag-pink">#Rest Api</div>
+            <div className="tag tag-pink">Tailwind</div>
+            <div className="tag tag-pink">MongoDB</div>
+            <div className="tag tag-pink">Node.js/Express</div>
           </div>
         </div>
       </div>
