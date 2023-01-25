c83975af995fea2b6969e2ddf5fa98b4a8ebf3b0
IIHEXYII <vetletessem@gmail.com>
2023-01-25 15:08:08 +0100

    added more content

--- src/Components/Cv.jsx
+++ src/Components/Cv.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "../Styles/Cv.css";
 import placeholderImg from "../Assets/Images/me.jpg";
-import img1 from "../Assets/Images/ecplay.jpg";
+import img1 from "../Assets/Images/ecPlay.jpg";
 import img2 from "../Assets/Images/noroff.jpg";
+import img3 from "../Assets/Images/nova.jpg";
+import img4 from "../Assets/Images/jobLoop.jpg";
 
 export default function Cv() {
   return (
@@ -16,7 +18,7 @@ export default function Cv() {
             <div className="cv-card-content">
               <h3>Junior front-end developer</h3>
               <p className="cv-card-under-title">
-                EC-Play <span>·</span> Sommer Vikar
+                EC-Play <span>·</span> Seasonal
               </p>
               <p className="cv-card-info">Jun 2021 - Aug 2021 · 3 mos</p>
               <p className="cv-card-info">Bergen, Norge</p>
@@ -28,22 +30,26 @@ export default function Cv() {
             </div>
           </div>
           <div className="cv-card">
-            <img src={placeholderImg} alt="company logo" />
+            <img src={img3} alt="company logo" />
             <div className="cv-card-content">
-              <h3>Jobb title</h3>
+              <h3>Frivilig</h3>
               <p className="cv-card-under-title">
-                Company <span>·</span> Stilling
+                NOVA Spektrum <span>·</span> Seasonal
               </p>
-              <p className="cv-card-info">Jun 2021 - Aug 2021 · 3 mos</p>
-              <p className="cv-card-info">Bergen, Norge</p>
+              <p className="cv-card-info">Nov 2014 - Nov 2022 </p>
+              <p className="cv-card-info">Lillestrøm, Viken, Norway</p>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptatibus optio dicta dolor tenetur eius. Minus doloribus
-                magnam, aliquid ratione et dicta! Aperiam autem harum illum
-                doloremque labore fugit.
+                Har jobbet på Spillexpo I flere år som frivilig. jeg reiser
+                tilbake vært år for å delta, nå er jeg med som frivilig leder
+                for andre frivilige og hjelper til 110% i de 3 dagene det pågår
+                sånn at Spillexpo er og blir en fantastik opplevelse for så
+                mange som mulig, for både gjester og frivilige.
               </p>
             </div>
           </div>
+          <button id="see-more-button">TEST</button>
+          <div id="more-content">YOU SEE ME!</div>
+
           <h2 className="cv-title">Utdanning</h2>
           <div className="utdanning">
             <div className="cv-card">
@@ -58,6 +64,26 @@ export default function Cv() {
                 <p>
                   Lærte basic nivå i design, HTML5, CSS, Javascript, React,
                   Rest-API, Word-press, og Typography bare for å nevne noe.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="utdanning">
+            <div className="cv-card">
+              <img src={img4} alt="company logo" />
+              <div className="cv-card-content">
+                <h3>KodeHode</h3>
+                <p className="cv-card-under-title">
+                  Kurs <span>·</span> Front-End Development
+                </p>
+                <p className="cv-card-info">Sep 2022</p>
+                <p className="cv-card-info">Bergen, Norge</p>
+                <p>
+                  Et fantastik kurs hvor jeg har lært en hel del om front-end
+                  relatert kunnskap og hvordan jobbe i gruppe under prosjekter
+                  med andre igjennom github. Var også innom et lite tileggs kurs
+                  innen backend hvor jeg Lærte om node.js, SQL, Express og
+                  MongoDB.
                 </p>
               </div>
             </div>
