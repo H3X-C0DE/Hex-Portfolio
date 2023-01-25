7058e0dac51db65d9716f36e16fe13c99466893a
IIHEXYII <vetletessem@gmail.com>
2023-01-25 15:09:23 +0100

    Fluff

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,16 @@ export default function Footer() {
       <div className="footer-container">
         <Social />
         <div className="page-end">
-          <span>© 2023 Vetle Tessem</span>
+          <span>
+            <a
+              className="tooltip"
+              href="https://github.com/H3X-C0DE/Hex-Portfolio"
+            >
+              <i class="fa-solid fa-heart "></i>
+              <span class="tooltiptext">( ͡~ ͜ʖ ͡° )</span>
+              <span> © 2023 Vetle Tessem</span>
+            </a>
+          </span>
         </div>
       </div>
     </section>
