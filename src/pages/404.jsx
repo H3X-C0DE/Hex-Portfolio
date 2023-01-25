80460b71dfb6ec3b86b02c4235b34cee7e45f04b
IIHEXYII <vetletessem@gmail.com>
2023-01-25 15:09:02 +0100

    Added Routes & 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import Social from "../Components/Social";
+import "../Styles/HeroSection.css";
+import "../Styles/404.css";
+import AnimatedBg from "../Components/misc/AnimatedBg";
 
-export default function page404() {
-  return <div>page404</div>;
+export default function HeroSection() {
+  return (
+    <div id="top-home" className="main-container">
+      <div className="container">
+        <div className="banner-container"></div>
+        <div className="info-container">
+          <h1 className="noPage-title">404</h1>
+          {/* <h2 className="under-title">&lt; Could not find the page! /&gt;</h2> */}
+          <h2>&lt; Could not find the page! /&gt;</h2>
+          <Link className="goBack404" to="/">
+            {" "}
+            Go Back{" "}
+          </Link>
+        </div>
+      </div>
+      <AnimatedBg />
+    </div>
+  );
 }
