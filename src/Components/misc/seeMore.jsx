fc0551f902e247a30454b03f05771ae072eeeacf
IIHEXYII <vetletessem@gmail.com>
2023-02-06 15:05:05 +0100

    added makeshift dropdown

--- src/Components/misc/seeMore.jsx
+++ src/Components/misc/seeMore.jsx
@@ -4,8 +4,10 @@ function SeeMore({ children }) {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div className="seeMore">
-      {isExpanded ? children : null}
+    <div className="seeMore-component">
+      <div className={isExpanded ? "seeMore active" : "seeMore collapsed"}>
+        {children}
+      </div>
       <div className="seeMore-button">
         <button onClick={() => setIsExpanded(!isExpanded)}>
           {isExpanded ? "See Less" : "See More"}
