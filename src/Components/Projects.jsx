caa230fe1668e8b0c68e7bcb09ac6592c37c5eec
IIHEXYII <vetletessem@gmail.com>
2023-02-08 08:58:18 +0100

    Improved seeMore function

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import "../Styles/Projects.css";
+import SeeMore from "./misc/seeMore";
+// images
 import berserkImage from "../Assets/Images/projects/ParallaxBerserk.jpg";
 import securityGenImage from "../Assets/Images/projects/404Security.jpg";
 import soundPad from "../Assets/Images/projects/soundPad.jpg";
 import youAreAwesome from "../Assets/Images/projects/youareawesome.jpg";
 import theDarkGalaxy from "../Assets/Images/projects/thedarkgalaxy.jpg";
+import newYears from "../Assets/Images/projects/newYear.png";
+import rickAndMorty from "../Assets/Images/projects/rickAndMorty.png";
+
 export default function Projects() {
-  // TODO:add rel="noreferrer" to all a tags
   return (
     <section id="portfolio">
       <div className="content-wrapper">
         <h2 className="about-title">Portfolio</h2>
-
         <div className="news-card">
           <a
             target="_blank"
@@ -39,7 +42,6 @@ export default function Projects() {
             </div>
           </div>
         </div>
-
         <div className="news-card">
           <a
             target="_blank"
@@ -69,7 +71,6 @@ export default function Projects() {
             </div>
           </div>
         </div>
-
         <div className="news-card">
           <a
             target="_blank"
@@ -155,6 +156,71 @@ export default function Projects() {
           </div>
         </div>
       </div>
+      <section className="seeMore-projects">
+        <SeeMore>
+          <div className="seeMore-wrapper">
+            <div className="news-card">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://thedarkgalaxy.netlify.app"
+                className="news-card__card-link"
+              >
+                NewYear Countdown
+              </a>
+              <img src={newYears} alt="" className="news-card__image" />
+              <div className="news-card__text-wrapper">
+                <h2 className="news-card__title">NewYear Countdown</h2>
+                <div className="news-card__post-date">Dec 22, 2022</div>
+                <div className="news-card__details-wrapper">
+                  <p className="news-card__excerpt">
+                    Originally this was meant to be a Countdown to christmas but
+                    by the time It was almost new year so I switched it out so
+                    it would be a yearly Countdown instead of just to
+                    christmas.&hellip;
+                  </p>
+                  <a
+                    href="https://thedarkgalaxy.netlify.app"
+                    className="news-card__read-more"
+                  >
+                    Visit page <i className="fas fa-long-arrow-alt-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="news-card">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://thedarkgalaxy.netlify.app"
+                className="news-card__card-link"
+              >
+                The Dark Galaxy
+              </a>
+              <img src={rickAndMorty} alt="" className="news-card__image" />
+              <div className="news-card__text-wrapper">
+                <h2 className="news-card__title">The Dark Galaxy</h2>
+                <div className="news-card__post-date">Oct 18, 2022</div>
+                <div className="news-card__details-wrapper">
+                  <p className="news-card__excerpt">
+                    This was a group project, we had around October hench the
+                    dark and gloomy theme of the site my roll in the project
+                    went from only making landing page to making the contact
+                    page as well as making all pages have the same consistency
+                    throughout all pages&hellip;
+                  </p>
+                  <a
+                    href="https://thedarkgalaxy.netlify.app"
+                    className="news-card__read-more"
+                  >
+                    Visit page <i className="fas fa-long-arrow-alt-right"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SeeMore>
+      </section>
     </section>
   );
 }
