2dd2401246cbd2640c8bc2668f590c0768d4c72f
IIHEXYII <vetletessem@gmail.com>
2023-02-09 14:23:03 +0100

    Updated Navbar + new features

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -18,12 +18,44 @@ export default function Dropdown({ toggled, onClick }) {
     document.addEventListener("mousedown", handleClickOutside);
   }, [dropdownRef]);
 
+  const [activeSection, setActiveSection] = useState("home");
+  const sections = ["home", "about", "cv", "portfolio", "contact"];
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sectionOffsets = {
+        home: 0,
+        about: document.querySelector("#about").offsetTop - 50,
+        cv: document.querySelector("#cv").offsetTop - 50,
+        portfolio: document.querySelector("#portfolio").offsetTop - 50,
+        contact: document.querySelector("#contact").offsetTop - 50,
+      };
+
+      const scrollPosition = window.pageYOffset;
+
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+
+        if (scrollPosition >= sectionOffsets[section]) {
+          setActiveSection(section);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="menu-container" ref={dropdownRef}>
       {/* <button onClick={buttonOnClick} className=""> */}
       <span>
         {/* <i className="fa-solid fa-burger"></i> */}
-        <label htmlFor="burgerCheck" className="fa-solid fa-burger"></label>
+        <label htmlFor="burgerCheck" className="fa-solid fa-burger">
+          <span>burger menu</span>
+        </label>
         <input
           id="burgerCheck"
           type="checkbox"
@@ -32,33 +64,24 @@ export default function Dropdown({ toggled, onClick }) {
         />
       </span>
       {/* </button> */}
-      <nav className={`menu ${isToggled ? "active" : "inactive"}`}>
-        <ul>
-          <li>
-            <a className="navbar-link" href="#home">
-              Hjem
-            </a>
-          </li>
-          <li>
-            <a className="navbar-link" href="#about">
-              Om&nbsp;Meg
-            </a>
-          </li>
-          <li>
-            <a className="navbar-link" href="#cv">
-              Cv
-            </a>
-          </li>
-          <li>
-            <a className="navbar-link" href="#portfolio">
-              Portefølje
-            </a>
-          </li>
-          <li>
-            <a className="navbar-link" href="#contact">
-              Kontakt
-            </a>
-          </li>
+      <nav className={`menu ${isToggled ? "isOpen" : "isClosed"}`}>
+        <ul className="navbar-list">
+          {sections.map((section) => (
+            <li
+              className={`navbar-item-menu ${
+                activeSection === section ? "active-menu" : ""
+              }`}
+              key={section}
+            >
+              <a className="navbar-link" href={`#${section}`}>
+                {section === "home" ? "Hjem" : ""}
+                {section === "about" ? "Info" : ""}
+                {section === "cv" ? "Cv" : ""}
+                {section === "portfolio" ? "Portefølje" : ""}
+                {section === "contact" ? "Kontakt" : ""}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </div>
