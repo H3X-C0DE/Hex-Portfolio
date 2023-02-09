d865998b621664e4b6b11f107e008c9e400f54a7
IIHEXYII <vetletessem@gmail.com>
2023-02-09 14:23:24 +0100

    updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,17 +6,49 @@ export default function Footer() {
   return (
     <section id="Footer">
       <div className="footer-container">
-        <Social />
         <div className="page-end">
-          <div>
-            <a href="https://github.com/H3X-C0DE/Hex-Portfolio">
-              <div className="tooltip">
-                <i className="fa-solid fa-heart fa-beat"></i>
-                <span className="tooltiptext">( ͡~ ͜ʖ ͡° )</span>
-              </div>
-              <p> © 2023 Vetle Tessem</p>
-            </a>
-          </div>
+          <a href="https://github.com/H3X-C0DE/Hex-Portfolio">
+            <div className="tooltip">
+              <i className="fa-solid fa-heart fa-beat"></i>
+              <span className="tooltiptext">( ͡~ ͜ʖ ͡° )</span>
+            </div>
+            <p> © 2023 Vetle Tessem</p>
+          </a>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/H3X-C0DE"
+              >
+                <i className="fa fa-lg fa-github">
+                  <span className="link-text">github</span>
+                </i>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/vetle-tessem/"
+              >
+                <i className="fa fa-lg fa-linkedin">
+                  <span className="link-text">linkedin</span>
+                </i>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/iihexyii/"
+              >
+                <i className="fa fa-lg fa-instagram">
+                  <span className="link-text">instagram</span>
+                </i>
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
