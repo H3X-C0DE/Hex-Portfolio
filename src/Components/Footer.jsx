dc6115273a5b45f5140bfdc90da48abb37d800b9
IIHEXYII <vetletessem@gmail.com>
2023-02-13 09:41:00 +0100

    fixed the damn gap bug...

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Social from "./Social";
 import "../Styles/footer.css";
 
 export default function Footer() {
@@ -14,41 +13,37 @@ export default function Footer() {
             </div>
             <p> © 2023 Vetle Tessem</p>
           </a>
-          {/* <ul>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://github.com/H3X-C0DE"
-              >
-                <i className="fa fa-lg fa-github">
-                  <span className="link-text">github</span>
-                </i>
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/vetle-tessem/"
-              >
-                <i className="fa fa-lg fa-linkedin">
-                  <span className="link-text">linkedin</span>
-                </i>
-              </a>
-            </li>
-            <li>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.instagram.com/iihexyii/"
-              >
-                <i className="fa fa-lg fa-instagram">
-                  <span className="link-text">instagram</span>
-                </i>
-              </a>
-            </li>
-          </ul> */}
+          <div className="footer-SoM">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/H3X-C0DE"
+            >
+              <i className="fa fa-lg fa-github">
+                <span className="link-text">github</span>
+              </i>
+            </a>
+
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/vetle-tessem/"
+            >
+              <i className="fa fa-lg fa-linkedin">
+                <span className="link-text">linkedin</span>
+              </i>
+            </a>
+
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.instagram.com/iihexyii/"
+            >
+              <i className="fa fa-lg fa-instagram">
+                <span className="link-text">instagram</span>
+              </i>
+            </a>
+          </div>
         </div>
       </div>
     </section>
